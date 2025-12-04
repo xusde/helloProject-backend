357428de7b1c7b4288c210cb2bf29dec1da33945
XuSDE <xpc6186@gmail.com>
2025-12-03 23:50:06 -0800

    update to v2

--- src/main/java/com/javaplay/helloproject/controller/HelloController.java
+++ src/main/java/com/javaplay/helloproject/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/test")
     public String test() {
-        return "hello";
+        return "hello v2";
     }
 }
